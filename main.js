33ebe438ea6e83f2ae5365eeb7aac045078005bd
Marita0 <mt.ovalleb@gmail.com>
2023-07-20 22:29:06 -0300

    cambios

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 const menuItems = document.querySelectorAll('.menu-item')
 
-menuItems.forEach(function(item) {
-    item.addEventListener('click', function (e) {
-        const currentItem = document.querySelector('.active');
-        currentItem.classList.remove('active');
-        e.target.classList.add('active');
-    });
+menuItems.forEach(function (item) {
+  item.addEventListener('click', function (e) {
+    const currentItem = document.querySelector('.active');
+    currentItem.classList.remove('active');
+    e.target.classList.add('active');
+  });
 });
+
+const elementoH1 = document.querySelector('#saludo');
+
+const textoBienvenida = 'Hola! Soy Jose Ovalle';
+let currentText = "";
+
+for (let index = 0 ; index < textoBienvenida.length; index ++)
+{
+  const letra = textoBienvenida[index];
+  setTimeout(() => {
+    currentText = currentText + letra;
+    elementoH1.innerText = currentText;
+  }, 100 * index);
+}
